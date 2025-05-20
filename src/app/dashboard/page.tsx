69f1c3e4d9d35ed97bc13a88f6b036268ee6bee1
Hamza <sougouhamza@gmail.com>
2025-05-20 23:47:47 +0000

    Prisma Config & User Model

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  //Authentification
+
+  //Si le compte n'existe pas
+
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
